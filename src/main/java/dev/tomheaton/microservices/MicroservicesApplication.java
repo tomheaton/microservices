58578b790fe810b741569eba56432cb6416370a2
= <thomas.m.heaton@gmail.com>
2022-04-18 21:07:55 +0100

    first route

--- src/main/java/dev/tomheaton/microservices/MicroservicesApplication.java
+++ src/main/java/dev/tomheaton/microservices/MicroservicesApplication.java
@@ -2,12 +2,20 @@ package dev.tomheaton.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MicroservicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return String.format("hello %s", name);
+    }
 }
