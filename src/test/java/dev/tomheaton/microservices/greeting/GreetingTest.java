6df8783849bd30ff88a54329dced7e74609252ec
= <thomas.m.heaton@gmail.com>
2022-04-19 23:22:16 +0100

    add tests?

---
+++ src/test/java/dev/tomheaton/microservices/greeting/GreetingTest.java
@@ -0,0 +1,7 @@
+package dev.tomheaton.microservices.greeting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+}
---
+++ src/test/java/dev/tomheaton/microservices/user/UserControllerTest.java
@@ -0,0 +1,7 @@
+package dev.tomheaton.microservices.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+}
