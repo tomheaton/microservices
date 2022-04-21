1fc675d52b725ed34af94bd6814b936924a35082
= <thomas.m.heaton@gmail.com>
2022-04-22 00:53:10 +0100

    post/put/delete code

--- src/main/java/dev/tomheaton/microservices/user/UserController.java
+++ src/main/java/dev/tomheaton/microservices/user/UserController.java
@@ -1,12 +1,8 @@
 package dev.tomheaton.microservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -23,5 +19,26 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @PostMapping
+    public void registerNewUser(@RequestBody User user) {
+        userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("userId") Long userId,
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String birthday
+    ) {
+        userService.updateStudent(userId, firstName, lastName, email, birthday);
     }
 }
--- src/main/java/dev/tomheaton/microservices/user/UserService.java
+++ src/main/java/dev/tomheaton/microservices/user/UserService.java
@@ -2,8 +2,12 @@ package dev.tomheaton.microservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +21,55 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public void addNewUser(User user) {
+        Optional<User> userOptional = userRepository.findUserByEmail(user.getEmail());
+
+        if (userOptional.isPresent()) {
+            throw new IllegalStateException("Email already exists.");
+        }
+
+        userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+        boolean exists = userRepository.existsById(userId);
+
+        if (!exists) {
+            throw new IllegalStateException("User with id " + userId + " does not exist.");
+        }
+
+        userRepository.deleteById(userId);
+    }
+
+    @Transactional
+    public void updateStudent(Long userId, String firstName, String lastName, String email, String birthday) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalStateException("User with id " + userId + " does not exist.")
+        );
+
+        if (firstName != null && firstName.length() > 0 && !user.getFirstName().equals(firstName)) {
+            user.setFirstName(firstName);
+        }
+
+        if (lastName != null && lastName.length() > 0 && !user.getLastName().equals(lastName)) {
+            user.setLastName(lastName);
+        }
+
+        if (email != null && email.length() > 0 && !user.getEmail().equals(email)) {
+            Optional<User> userOptional = userRepository.findUserByEmail(email);
+
+            if (userOptional.isPresent()) {
+                throw new IllegalStateException("Email already exists.");
+            }
+
+            user.setEmail(email);
+        }
+
+        // TODO: fix date parsing.
+        /*if (birthday != null && birthday.length() > 0 && !user.getBirthday().equals(birthday)) {
+            user.setBirthday(LocalDate.parse(birthday));
+        }*/
     }
 }
