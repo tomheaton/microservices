304b36e527d1a857d4ad7acc8fb71a53510f8438
Tom Heaton <thomas.m.heaton@gmail.com>
2022-04-28 20:37:48 +0100

    security config

---
+++ src/main/java/dev/tomheaton/microservices/security/ApplicationSecurityConfig.java
@@ -0,0 +1,7 @@
+package dev.tomheaton.microservices.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
