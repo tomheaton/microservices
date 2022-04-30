0f6bcc57e683aba3239aa5701c575ee113551181
Tom Heaton <thomas.m.heaton@gmail.com>
2022-04-30 23:34:06 +0100

    security config work :lock:

--- src/main/java/dev/tomheaton/microservices/security/SecurityConfig.java
+++ src/main/java/dev/tomheaton/microservices/security/SecurityConfig.java
@@ -1,18 +1,49 @@
 package dev.tomheaton.microservices.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+//                .antMatchers("/", "index", "/css/*", "/js/*")
+//                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
     }
 
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("tomheaton")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
