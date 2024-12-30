50e8621ed9f7f6a4e6fc91338b7dae28ab72f842
Tom Heaton <thomas.m.heaton@gmail.com>
2024-12-30 15:00:19 +0000

    chore: update spring from v3.0.0-M2 to v3.0.0.0

--- src/main/java/dev/tomheaton/microservices/security/SecurityConfig.java
+++ src/main/java/dev/tomheaton/microservices/security/SecurityConfig.java
@@ -5,18 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
 
@@ -25,20 +25,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-//                .antMatchers("/", "index", "/css/*", "/js/*")
-//                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                                .anyRequest().authenticated()
+                )
                 .httpBasic();
+        return http.build();
     }
 
     @Bean
-    @Override
     protected UserDetailsService userDetailsService() {
         UserDetails tomUser = User.builder()
                 .username("tomheaton")
